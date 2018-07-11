2b9b1ff653fd39db964eb23d4df7a992453e6686
Lakshmiprasad <slakshmiprasad93@gmail.com>
2018-07-12 02:11:12 +0530

    feat: cache statics using serviceWorker

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = "restaurant-review-cache-1";
+var urlsToCache = [
+  '/',
+  'index.html',
+  '/restaurant.html',
+  '/js',
+  '/css/styles.css',
+  '/css/responsive.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/data/restaurants.json',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+    return cache.addAll(urlsToCache);
+  }));
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(function (response) {
+    return response || fetch(event.request);
+  }));
+});
